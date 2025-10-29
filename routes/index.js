40038c656fdee2136c6186d3a4646260c7a3d327
Oluwatigbo <oluwatigboalao11@gmail.com>
2025-10-29 13:32:27 +0100

    updating get

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
+const express = require('express');
 const router = require('express').Router();
+
+const usersController = require('../controllers/users');
 
 router.get('/', (req, res) => { res.send('Hello World'); });
 
+router.use('/users', require('./users'));
+
 module.exports = router;
+
 
